37d700f652f987ebbb8387dcbc6ccdb310d27d2e
Tiago Polita <tiago@venda.com>
2026-02-05 06:28:06 -0500

    debug: add logs to main.jsx to track initialization

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from '@/App.jsx'
 import '@/index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+console.log('main.jsx: Starting React application');
+const rootElement = document.getElementById('root');
+console.log('main.jsx: Root element found:', !!rootElement);
+
+ReactDOM.createRoot(rootElement).render(
   // <React.StrictMode>
   <App />
   // </React.StrictMode>,
